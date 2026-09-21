65e6c1ae1da580afd97b5650560f9b11010a4c30
Amanda Bergman <adbergman@mail.mccneb.edu>
2026-09-21 23:15:09 +0000

    Spaced out code better to see

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const display = (label, value) =>
 // ADD YOUR CODE BELOW
 
 // TODO: Create variables for your name (string), total number of modules for our class (number), and if you're enrolled (boolean)
+
 const courseModules = ['Module 1', 'Module 2', 'Module 3', 'Module 4', 'Module 5', 'Module 6', 'Module 7', 'Module 8', 'Module 9', 'Module 10'];
 const completedModules = ['Module 1', 'Module 2'];
 const myName = "Amanda Bergman";
@@ -25,25 +26,30 @@ let totalModules = 10;
 let isEnrolled = true;
 
 // TODO: Use a template literal to output a welcome message. Use at least one ${}.
+
 const welcomeMessage = `Welcome, to ${myName}'s Webpage!`;
   console.log(`Welcome, to ${myName}'s Webpage!`);
 
 // TODO: Calculate the total study hours for the course. There are 10 modules. Each module takes roughly 6 hours.
 // Formula: totalStudyHours = totalModules * hoursPerWeek
+
 const hoursPerWeek = 6;
 const totalStudyHours = totalModules * hoursPerWeek;
 
 // TODO: Calculate the number of study hours each day. Convert the output to minutes (this formula is not provided).
 // Formula: dailyStudyHours = hoursPerWeek / 7
+
 const dailyStudyHours = hoursPerWeek / 7;
 const dailyStudyMinutes = dailyStudyHours * 60;
 
 // TODO: Give yourself a rest day and exclude one day out of your week. Calculate the new number of hours and set it to adjustedDailyHours. Convert the output to minutes (this formula is not provided).
+
 let adjustedDailyHours = hoursPerWeek / 6;
 let adjustedDailyMinutes = adjustedDailyHours * 60;
 
 // TODO: Calculate the course percent complete and the course percent remaining. Imagine you've completed 2 modules (Start Here and Module 1).
 // Formula: percent = (part / whole) * 100
+
 let modulesCompleted = parseInt(prompt('Enter the number of modules you have completed (0-10):'));
 let percentComplete = (modulesCompleted / totalModules) * 100;
 let modulesRemaining = totalModules - modulesCompleted;
